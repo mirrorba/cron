569f627737352399eef2b8a6a360ae24b6f8663f
Emir Beganovic <beganovic.emir@gmail.com>
2018-07-22 11:33:35 +0400

    Remove Debian

--- debian-cd.sh
+++
@@ -1,3 +0,0 @@
-#/bin/dash
-umask 022; rsync -avz --delete --safe-links mirror.de.leaseweb.net::debian-cd/ /var/www/debian-cd --stats --progress --times --links --hard-links --partial --block-size=8192 --recursive --times --links --hard-links --delete
-date -u > /var/www/debian-cd/project/trace/mirror.ba
