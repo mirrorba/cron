e61f13a0e2f0d81b8e966999f5947b4cd167109b
Emir Beganovic <beganovic.emir@gmail.com>
2018-09-23 15:45:00 +0400

    Update

---
+++ archlinux.sh
@@ -0,0 +1,1 @@
+rsync -rtlvH --delete-after --delay-updates --safe-links rsync://mirror.23media.de/archlinux/ /var/www/mirror.ba/archlinux
---
+++ centos.sh
@@ -0,0 +1,1 @@
+rsync -azH --delete eu-msync.centos.org::CentOS /var/www/mirror.ba/centos --stats --progress
